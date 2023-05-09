2591efa4f4ef65c7dbb0e254ff072706d7309dcb
Hakan Çevik <hakanxcevik7@gmail.com>
2023-05-09 13:35:44 +0300

    Bottom Navigation and required fragments have been added

--- app/src/main/java/com/hakancevik/newsappbihaber/MainActivity.kt
+++ app/src/main/java/com/hakancevik/newsappbihaber/MainActivity.kt
@@ -1,11 +1,25 @@
 package com.hakancevik.newsappbihaber
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.Navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
+
+import com.hakancevik.newsappbihaber.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navController = findNavController(this, R.id.fragment)
+        binding.bottomNavigationView.setupWithNavController(navController)
+
+
     }
 }
